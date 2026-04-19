ec0930b3b69643343af2a3417080042b90b9232f
Danny Seo <sks8410@naver.com>
2026-04-19 15:42:07 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // [Why] GitHub Pages는 프로젝트를 루트가 아닌 서브 디렉토리
+  // (https://{user}.github.io/{repo-name}/)에서 서빙한다.
+  // base를 지정하지 않으면 빌드된 에셋 경로가 '/'로 생성되어 404가 발생한다.
+  base: '/time_tracking_dashboard/',
 });
